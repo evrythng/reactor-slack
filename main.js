de8738b22f50d675c06811c13d3ad7c1c7bd7bb1
C-D-Lewis <Bonsitm@gmail.com>
2018-05-21 09:24:38 +0100

    Log the inputs, minor fixes

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const request = require('request');
 
 const WEBHOOK_URL = '';
 
-const validate = ({ customFields }) => {
+const validate = (customFields) => {
     const { channel, text } = customFields;
     if (!channel) throw new Error('channel not specified!');
     if (!text) throw new Error('text not specified!');
     
+    logger.info(JSON.stringify(customFields));
     return Promise.resolve(customFields);
 };
 
-const postToSlack = json => new Promise((resolve) => {
+const postToSlack = json => new Promise((resolve, reject) => {
   request.post({ url: WEBHOOK_URL, json }, (err, response, body) => {
-    if (err) throw err;
+    if (err) {
+      reject(err);
+      return;
+    }
 
     resolve(body);
   });  
@@ -20,7 +24,7 @@ const postToSlack = json => new Promise((resolve) => {
 
 // @filter(onActionCreated) action.type=_PostToSlack
 function onActionCreated(event) {
-  validate(event.action)
+  validate(event.action.customFields)
     .then(postToSlack)
     .then(body => logger.info(`Response from Slack: ${body}`))
     .catch(err => logger.error(err))
